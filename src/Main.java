3ca197632aee3529bd014ff30cf3d848586081cd
Vaughn <vwalker04@yahoo.com>
2018-04-20 20:08:03 -0400

    Added an infinite loop

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        for (int i = 0; i < i.length; i++) {
+            System.out.println("Hello World!");
+        }
     }
 }
